28afef3a41480270a366b138988b589b47400eee
Alex-Lanser <alanser@icloud.com>
2023-06-22 09:26:08 -0500

    Removed submit.js

--- submit.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('submit-btn').addEventListener('click', function () {
-    setTimeout(function () {
-        // Message sent, show alert
-        alert('Message sent successfully!');
-    }, 1000);
-});
