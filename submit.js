47c5da4c49771649322fa5021d5330e27f010bc6
Alex-Lanser <alanser@icloud.com>
2023-06-22 09:24:50 -0500

    Submit alert

---
+++ submit.js
@@ -0,0 +1,6 @@
+document.getElementById('submit-btn').addEventListener('click', function () {
+    setTimeout(function () {
+        // Message sent, show alert
+        alert('Message sent successfully!');
+    }, 1000);
+});
